84b0dcc2f053c87e85d15177d8fa4565a47f54e9
Ram <sriramkumar.gb@gmail.com>
2024-03-03 19:00:44 -0800

    solution for problem 7

---
+++ haskell-99/7/solution.hs
@@ -0,0 +1,9 @@
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten (Elem x) = [x]
+flatten (List []) = []
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+
+main :: IO ()
+main = return ()
