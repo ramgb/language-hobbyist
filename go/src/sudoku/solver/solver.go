af20f97a37b4d7b4fda4d64564f0037267eb02a5
ramgb <sriramkumar.gb@gmail.com>
2024-11-30 14:20:41 -0800

    Intermediate of solver using undo updates

--- go/src/sudoku/solver/solver.go
+++ go/src/sudoku/solver/solver.go
@@ -8,6 +8,7 @@ import (
 
 type solver interface {
 	Solve() float64
+	// Implement PrintMetrics() method
 }
 
 type SimpleSolver struct {
@@ -44,19 +45,26 @@ func NewSimpleSolver(s *sudoku.Sudoku) *SimpleSolver {
 	}
 }
 
-func updateValue(x int, y int, value int, guesses *[9][9]map[int]bool) bool {
+func updateValue(x int, y int, value int, guesses *[9][9]map[int]bool) (bool, [9][9]map[int]bool) {
 	guesses[x][y] = map[int]bool{value: true}
+	changed := [9][9]map[int]bool{}
 	for i := 0; i < 9; i++ {
 		if i != y {
+			if guesses[x][i][value] {
+				changed[x][i][value] = true
+			}
 			delete(guesses[x][i], value)
 			if len(guesses[x][i]) == 0 {
-				return false
+				return false, changed
 			}
 		}
 		if i != x {
+			if guesses[i][y][value] {
+				changed[i][y][value] = true
+			}
 			delete(guesses[i][y], value)
 			if len(guesses[i][y]) == 0 {
-				return false
+				return false, changed
 			}
 		}
 	}
@@ -66,14 +74,21 @@ func updateValue(x int, y int, value int, guesses *[9][9]map[int]bool) bool {
 	for i := x0; i < x0+3; i++ {
 		for j := y0; j < y0+3; j++ {
 			if i != x && j != y {
+				if guesses[i][j][value] {
+					changed[i][j][value] = true
+				}
 				delete(guesses[i][j], value)
 				if len(guesses[i][j]) == 0 {
-					return false
+					return false, changed
 				}
 			}
 		}
 	}
-	return true
+	return true, changed
+}
+
+func (s *SimpleSolver) undoUpdate(changed [9][9]map[int]bool) {
+	// TODO - undo the update
 }
 
 // For Debugging - remove before productionizing
@@ -90,10 +105,33 @@ func (s *SimpleSolver) printGuesses() {
 	}
 }
 
+func (s *SimpleSolver) solveInternal(x int, y int) bool {
+	// boundary condition - move to next row
+	if y == 9 {
+		return s.solveInternal(x+1, 0)
+	}
+	// boundary condition - end of board
+	if x == 9 {
+		return true
+	}
+	guesses := s.guesses[x][y]
+	for guess := range guesses {
+		updateValue(x, y, guess, &s.guesses)
+		if s.solveInternal(x, y+1) {
+			return true
+		}
+	}
+	s.guesses[x][y] = guesses
+	s.solveInternal(x, y+1)
+	return false
+}
+
 // Main function to solve the Sudoku
 func (s *SimpleSolver) Solve() float64 {
 	start := time.Now()
 
+	solved := s.solveInternal(0, 0)
+	print("Solved: ", solved)
 	// TODO: Implement the internal solve method
 	s.printGuesses()
 	return time.Since(start).Seconds()
