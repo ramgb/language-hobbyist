b969346cfb8b8c157d848b9f257860833a7c59df
ramgb <sriramkumar.gb@gmail.com>
2024-11-30 16:16:43 -0800

    Intermediate broken solution + w. validation

--- go/src/sudoku/solver/solver.go
+++ go/src/sudoku/solver/solver.go
@@ -31,7 +31,7 @@ func NewSimpleSolver(s *sudoku.Sudoku) *SimpleSolver {
 	for i := 0; i < 9; i++ {
 		for j := 0; j < 9; j++ {
 			if board[i][j] != 0 {
-				updated := updateValue(i, j, board[i][j], &guesses)
+				updated, _ := updateValue(i, j, board[i][j], &guesses)
 				if !updated {
 					panic("Invalid board")
 				}
@@ -49,6 +49,12 @@ func updateValue(x int, y int, value int, guesses *[9][9]map[int]bool) (bool, [9
 	guesses[x][y] = map[int]bool{value: true}
 	changed := [9][9]map[int]bool{}
 	for i := 0; i < 9; i++ {
+		if changed[x][i] == nil {
+			changed[x][i] = map[int]bool{}
+		}
+		if changed[i][y] == nil {
+			changed[i][y] = map[int]bool{}
+		}
 		if i != y {
 			if guesses[x][i][value] {
 				changed[x][i][value] = true
@@ -73,6 +79,9 @@ func updateValue(x int, y int, value int, guesses *[9][9]map[int]bool) (bool, [9
 	y0 := y / 3 * 3
 	for i := x0; i < x0+3; i++ {
 		for j := y0; j < y0+3; j++ {
+			if changed[i][j] == nil {
+				changed[i][j] = map[int]bool{}
+			}
 			if i != x && j != y {
 				if guesses[i][j][value] {
 					changed[i][j][value] = true
@@ -88,21 +97,50 @@ func updateValue(x int, y int, value int, guesses *[9][9]map[int]bool) (bool, [9
 }
 
 func (s *SimpleSolver) undoUpdate(changed [9][9]map[int]bool) {
-	// TODO - undo the update
-}
-
-// For Debugging - remove before productionizing
-// Print the number of guesses for each cell
-func (s *SimpleSolver) printGuesses() {
 	for i := 0; i < 9; i++ {
 		for j := 0; j < 9; j++ {
-			fmt.Print(len(s.guesses[i][j]))
-			if j < 8 {
-				fmt.Print(" ")
+			for value := range changed[i][j] {
+				s.guesses[i][j][value] = true
 			}
 		}
-		fmt.Println()
 	}
+}
+
+// Print the guessed board
+func (s *SimpleSolver) getSolvedBoard() [9][9]int {
+	board := [9][9]int{}
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			board[i][j] = getKey(s.guesses[i][j])
+		}
+	}
+	return board
+}
+
+func (s *SimpleSolver) validateSolution() bool {
+	board := s.getSolvedBoard()
+	sumRow := [9]int{}
+	sumCol := [9]int{}
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			sumRow[i] += board[i][j]
+			sumCol[i] += board[j][i]
+		}
+	}
+
+	for i := 0; i < 9; i++ {
+		if sumRow[i] != 45 || sumCol[i] != 45 {
+			return false
+		}
+	}
+	return true
+}
+
+func getKey(m map[int]bool) int {
+	for key := range m {
+		return key
+	}
+	return -1
 }
 
 func (s *SimpleSolver) solveInternal(x int, y int) bool {
@@ -115,14 +153,21 @@ func (s *SimpleSolver) solveInternal(x int, y int) bool {
 		return true
 	}
 	guesses := s.guesses[x][y]
-	for guess := range guesses {
-		updateValue(x, y, guess, &s.guesses)
-		if s.solveInternal(x, y+1) {
-			return true
+	if len(guesses) > 1 {
+		for guess := range guesses {
+			updated, changed := updateValue(x, y, guess, &s.guesses)
+			if updated {
+				if s.solveInternal(x, y+1) {
+					return true
+				}
+			}
+			s.undoUpdate(changed)
 		}
+		s.guesses[x][y] = guesses
+	} else if len(guesses) == 1 {
+		return s.solveInternal(x, y+1)
 	}
-	s.guesses[x][y] = guesses
-	s.solveInternal(x, y+1)
+
 	return false
 }
 
@@ -131,8 +176,8 @@ func (s *SimpleSolver) Solve() float64 {
 	start := time.Now()
 
 	solved := s.solveInternal(0, 0)
-	print("Solved: ", solved)
-	// TODO: Implement the internal solve method
-	s.printGuesses()
+	fmt.Println("Solved: ", solved)
+	fmt.Println("Valid: ", s.validateSolution())
+
 	return time.Since(start).Seconds()
 }
