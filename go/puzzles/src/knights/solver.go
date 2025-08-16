e2ab1154856eb1a1d788179f39cbd95feea05ec7
ramgb <sriramkumar.gb@gmail.com>
2025-08-16 13:00:13 -0700

    Knight's tour brute force solver

--- go/puzzles/src/knights/solver.go
+++ go/puzzles/src/knights/solver.go
@@ -6,10 +6,118 @@ import (
 )
 
 func solve(boardSize int) {
-	board := make([][]bool, boardSize)
+	board := make([][]int, boardSize)
 	for i := range board {
-		board[i] = make([]bool, boardSize)
+		board[i] = make([]int, boardSize)
 	}
+	solved := knightjump(board, 0, 0, 1)
+	if solved {
+		printBoard(board)
+	} else {
+		fmt.Println("Cant solve")
+	}
+}
+
+func printBoard(board [][]int) {
+	for i := range board {
+		for j := range board[i] {
+			fmt.Print(strconv.Itoa(board[i][j]) + "\t")
+		}
+		fmt.Println()
+	}
+}
+
+func knightjump(board [][]int, row, col, pos int) bool {
+	boardSize := len(board)
+
+	if pos > boardSize*boardSize {
+		return true
+	}
+	if board[row][col] != 0 {
+		return false
+	}
+
+	board[row][col] = pos
+
+	if pos == boardSize*boardSize {
+		return true
+	}
+	jumpSuccess := false
+
+	// Implement warnsdorff's rule using a priority queue
+	if row+1 < boardSize {
+		if col+2 < boardSize {
+			jumpSuccess = jumpSuccess || knightjump(board, row+1, col+2, pos+1)
+		}
+		if jumpSuccess {
+			return true
+		}
+
+		if col-2 >= 0 {
+			jumpSuccess = jumpSuccess || knightjump(board, row+1, col-2, pos+1)
+		}
+
+		if jumpSuccess {
+			return true
+		}
+	}
+
+	if row+2 < boardSize {
+
+		if col+1 < boardSize {
+			jumpSuccess = jumpSuccess || knightjump(board, row+2, col+1, pos+1)
+		}
+
+		if jumpSuccess {
+			return true
+		}
+
+		if col-1 >= 0 {
+			jumpSuccess = jumpSuccess || knightjump(board, row+2, col-1, pos+1)
+		}
+
+		if jumpSuccess {
+			return true
+		}
+	}
+
+	if row-1 >= 0 {
+		if col+2 < boardSize {
+			jumpSuccess = jumpSuccess || knightjump(board, row-1, col+2, pos+1)
+		}
+
+		if jumpSuccess {
+			return true
+		}
+		if col-2 >= 0 {
+			jumpSuccess = jumpSuccess || knightjump(board, row-1, col-2, pos+1)
+		}
+
+		if jumpSuccess {
+			return true
+		}
+	}
+
+	if row-2 >= 0 {
+		if col+1 < boardSize {
+			jumpSuccess = jumpSuccess || knightjump(board, row-2, col+1, pos+1)
+		}
+
+		if jumpSuccess {
+			return true
+		}
+
+		if col-1 >= 0 {
+			jumpSuccess = jumpSuccess || knightjump(board, row-2, col-1, pos+1)
+		}
+
+		if jumpSuccess {
+			return true
+		}
+	}
+
+	board[row][col] = 0
+	return false
 }
 
 func Start() {
