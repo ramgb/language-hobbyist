c04a7b3fd2c9eb59a188463f76097d106f837e8e
ramgb <sriramkumar.gb@gmail.com>
2025-08-16 15:00:15 -0700

    Implemented Warnsdorff's rule for Knight's Tour

--- go/puzzles/src/knights/solver.go
+++ go/puzzles/src/knights/solver.go
@@ -2,8 +2,15 @@ package knights
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 )
+
+type BestCell struct {
+	Row   int
+	Col   int
+	Score int
+}
 
 func solve(boardSize int) {
 	board := make([][]int, boardSize)
@@ -45,79 +52,114 @@ func knightjump(board [][]int, row, col, pos int) bool {
 	jumpSuccess := false
 
 	// Implement warnsdorff's rule using a priority queue
+	neighbors := getSortedNeighbors(board, row, col)
+
+	for index := range neighbors {
+		neighbor := neighbors[index]
+		if neighbor.Score != -1 {
+			jumpSuccess = jumpSuccess || knightjump(board, neighbor.Row, neighbor.Col, pos+1)
+			if jumpSuccess {
+				return true
+			}
+		}
+	}
+	board[row][col] = 0
+	return false
+}
+
+func getSortedNeighbors(board [][]int, row, col int) []BestCell {
+	neighbors := make([]BestCell, 8)
+	modifiers := []int{1, 2}
+	operators := []string{"+", "-"}
+	index := 0
+	for i := range modifiers {
+		for j := range operators {
+			for k := range operators {
+				rowIndex := executeOperation(row, modifiers[i], operators[j])
+				colIndex := executeOperation(col, modifiers[i^1], operators[k])
+				score := getScore(board, rowIndex, colIndex)
+				neighbors[index] = BestCell{Row: rowIndex, Col: colIndex, Score: score}
+				index++
+			}
+		}
+	}
+	sort.Slice(neighbors, func(i, j int) bool {
+		return neighbors[i].Score < neighbors[j].Score
+	})
+	return neighbors
+}
+
+func executeOperation(x int, y int, op string) int {
+	if op == "+" {
+		return x + y
+	}
+	if op == "-" {
+		return x - y
+	}
+	return -1
+}
+
+func getScore(board [][]int, row, col int) int {
+	if row < 0 || row >= len(board) || col < 0 || col >= len(board) {
+		return -1
+	}
+	score := 0
+	boardSize := len(board)
 	if row+1 < boardSize {
 		if col+2 < boardSize {
-			jumpSuccess = jumpSuccess || knightjump(board, row+1, col+2, pos+1)
-		}
-		if jumpSuccess {
-			return true
+			if board[row+1][col+2] == 0 {
+				score++
+			}
 		}
 
 		if col-2 >= 0 {
-			jumpSuccess = jumpSuccess || knightjump(board, row+1, col-2, pos+1)
-		}
-
-		if jumpSuccess {
-			return true
+			if board[row+1][col-2] == 0 {
+				score++
+			}
 		}
 	}
 
 	if row+2 < boardSize {
-
 		if col+1 < boardSize {
-			jumpSuccess = jumpSuccess || knightjump(board, row+2, col+1, pos+1)
-		}
-
-		if jumpSuccess {
-			return true
+			if board[row+2][col+1] == 0 {
+				score++
+			}
 		}
 
 		if col-1 >= 0 {
-			jumpSuccess = jumpSuccess || knightjump(board, row+2, col-1, pos+1)
-		}
-
-		if jumpSuccess {
-			return true
+			if board[row+2][col-1] == 0 {
+				score++
+			}
 		}
 	}
 
 	if row-1 >= 0 {
 		if col+2 < boardSize {
-			jumpSuccess = jumpSuccess || knightjump(board, row-1, col+2, pos+1)
-		}
-
-		if jumpSuccess {
-			return true
+			if board[row-1][col+2] == 0 {
+				score++
+			}
 		}
 		if col-2 >= 0 {
-			jumpSuccess = jumpSuccess || knightjump(board, row-1, col-2, pos+1)
-		}
-
-		if jumpSuccess {
-			return true
+			if board[row-1][col-2] == 0 {
+				score++
+			}
 		}
 	}
 
 	if row-2 >= 0 {
 		if col+1 < boardSize {
-			jumpSuccess = jumpSuccess || knightjump(board, row-2, col+1, pos+1)
-		}
-
-		if jumpSuccess {
-			return true
+			if board[row-2][col+1] == 0 {
+				score++
+			}
 		}
 
 		if col-1 >= 0 {
-			jumpSuccess = jumpSuccess || knightjump(board, row-2, col-1, pos+1)
-		}
-
-		if jumpSuccess {
-			return true
+			if board[row-2][col-1] == 0 {
+				score++
+			}
 		}
 	}
-
-	board[row][col] = 0
-	return false
+	return score
 }
 
 func Start() {
